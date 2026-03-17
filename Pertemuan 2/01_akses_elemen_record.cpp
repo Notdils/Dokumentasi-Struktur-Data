b20d20f42ac0a7e9684cf160ae94559f293f39ac
Yahdilil Haq Sarifuddin <yahdililhaq08@gmail.com>
2026-03-17 12:49:29 +0700

    Add files via upload

---
+++ Pertemuan 2/01_akses_elemen_record.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct mahasiswa {
+    string nim, nama, prodi;
+    float ipk;
+};
+
+int main() {
+    mahasiswa mhs1, mhs2;
+    mhs1.nim = "11223344";
+    cin >> mhs1.nama;
+    mhs2.nim = "11335577";
+    cin >> mhs2.ipk;
+    cout << "NIM mhs1: " << mhs1.nim << endl;
+    cout << "IPK mhs2: " << mhs2.ipk << endl;
+    return 0;
+}
---
+++ Pertemuan 2/02_definisi_array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai[4] = {10, 20, 30, 40};
+    for (int idx = 0; idx < 4; idx++) {
+        cout << nilai[idx] << endl;
+    }
+    return 0;
+}
